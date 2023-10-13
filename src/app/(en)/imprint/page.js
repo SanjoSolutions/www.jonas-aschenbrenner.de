e6075c86acce6f18be5d7b02f02adb4908fd30ff
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-10-14 00:06:25 +0200

    Adds title for "Imprint" and "Impressum" pages

--- src/app/(en)/imprint/page.js
+++ src/app/(en)/imprint/page.js
@@ -1,6 +1,10 @@
 import Imprint from "@/components/pages/imprint.mdx"
 import en from "../../../../dictionaries/en.json"
 
+export const metadata = {
+  title: "Imprint",
+}
+
 export default function (props) {
-  return <Imprint { ...props } dictionary={ en.imprint } />
+  return <Imprint {...props} dictionary={en.imprint} />
 }
