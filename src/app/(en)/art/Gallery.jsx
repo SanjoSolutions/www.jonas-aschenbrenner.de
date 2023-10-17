71149af282f6454d0559522e51fd967e65741043
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-10-17 14:37:48 +0200

    Margin bottom

--- src/app/(en)/art/Gallery.jsx
+++ src/app/(en)/art/Gallery.jsx
@@ -5,7 +5,7 @@ export function Gallery({ art }) {
   return (
     <div className="art-piece row">
       {art.map((path, index) => (
-        <div key={index} className="col-12 col-md-3">
+        <div key={index} className="col-12 col-md-3 mb-3">
           <a href={path} className="art-piece">
             <Image src={path} width={304} height={304} className="w-100" />
           </a>
